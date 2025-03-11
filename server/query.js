b400a516c3f6152fece68d0a1b7645408ad0dfe4
Coolthread80 <72284973+Coolthread80@users.noreply.github.com>
2025-03-11 02:59:47 -0600

    Update query.js

--- server/query.js
+++ server/query.js
@@ -4,10 +4,14 @@ const express = require('express');
 const router = express.Router();
 const { getConnection } = require('./db');
 const path = require('path');
+const util = require('util');
+
 // Página principal
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
 });
+
+//------------------------------------CONSULTAS DE EMPLEADOS------------------------------------
 
 //ver todos los empleados con rol
 router.post('/see-empleados', (req, res) => {
@@ -22,6 +26,32 @@ router.post('/see-empleados', (req, res) => {
     });
 });
 
+//Eliminar un usuario
+router.post('/delete-user', async (req, res)=> {
+    const { id } = req.body;
+    const cid = Number([id]);
+    const connection = getConnection();
+    const query = util.promisify(connection.query).bind(connection)
+    try{
+        const q2 = await query(
+            `DELETE FROM UsuariosRol
+            WHERE id_usuario = ? `
+            , cid);
+        const q1 = await query(
+            `DELETE FROM UsuariosNom
+            WHERE id_usuario = ? `
+            , cid);
+        res.json({message: "Usuario eliminado"});
+    } catch (error) {
+        console.error("Error al eliminar usuario:", error);
+        res.status(500).json({ error: "Error al eliminar usuario" });
+    } finally {
+        connection.end();
+    }
+});
+
+//------------------------------------CONSULTAS DE VENTAS/PEDIDOS------------------------------------
+
 //ver todas las ventas
 router.post('/see-ventas', (req, res) => {
     const connection = getConnection();
@@ -30,6 +60,20 @@ router.post('/see-ventas', (req, res) => {
         FROM Pedidos`,
         (error, results)=>{
         res.json({title: "Control de ventas", header:["ID del sistema","ID del alimento", "total del pedido"], dbresults: results});
+        connection.end();
+    });
+});
+
+//ver los pedidos más recientes
+router.post('/see-pedidos', (req, res)=>{
+    const connection = getConnection();
+    connection.query(`
+        SELECT *
+        FROM PedidoDetalles 
+        ORDER BY hora_pedido
+        LIMIT 5`,
+        (error, results)=>{
+        res.json({title: "Pedidos en Curso", header:["ID Pedido","Cliente", "Hora"], dbresults: results});
         connection.end();
     });
 });
